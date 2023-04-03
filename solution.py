1d4687e17954689919482eca7c3f5cc271398a82
Kirill <explorerpeople123@gmail.com>
2023-04-03 20:51:00 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -4,14 +4,14 @@ import numpy as np
 from scipy.stats import norm
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 134277149 # Ваш chat ID, не меняйте название переменной
 
 def solution(p: float, x: np.array) -> tuple:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
     alpha = 1 - p
-    loc = x.mean()
-    scale = np.sqrt(np.var(x)) / np.sqrt(len(x))
-    return loc - scale * norm.ppf(1 - alpha / 2), \
-           loc - scale * norm.ppf(alpha / 2)
+    n = len(x)
+    mean_x = np.mean(x)
+    s = np.sqrt(np.sum((x - mean_x) ** 2) / (n - 1))
+    t = norm.ppf(1 - alpha / 2)
+    left = mean_x - t * s / np.sqrt(n)
+    right = mean_x + t * s / np.sqrt(n)
+    return (left, right)
