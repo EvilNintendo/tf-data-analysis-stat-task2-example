258961640e2fca54edb869b56dabccf966ffb2c4
Kirill <explorerpeople123@gmail.com>
2023-04-03 21:22:48 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 
-from scipy.stats import t
+from scipy.stats import norm
 
 
 chat_id = 134277149 # Ваш chat ID, не меняйте название переменной
@@ -11,7 +11,7 @@ def solution(p: float, x: np.array) -> tuple:
     n = len(x)
     mean_x = np.mean(x)
     std_x = np.std(x, ddof=1)
-    t_alpha_2 = t.ppf(1 - alpha / 2, n - 1)
-    left_bound = mean_x - t_alpha_2 * std_x / np.sqrt(n)
-    right_bound = mean_x + t_alpha_2 * std_x / np.sqrt(n)
+    z_alpha_2 = norm.ppf(1 - alpha / 2)
+    left_bound = mean_x - z_alpha_2 * std_x / np.sqrt(n)
+    right_bound = mean_x + z_alpha_2 * std_x / np.sqrt(n)
     return (left_bound, right_bound)
